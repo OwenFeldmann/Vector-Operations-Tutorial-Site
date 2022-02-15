59e429d71048aa4a382aa7a90da5147cd5baa110
OwenFeldmann <ofeldman@unca.edu>
2022-02-15 17:34:50 -0500

    Reworked scalar vector multiplication tutorial functionality

--- Scalar-Vector Multiplication/ScalarVectorTutorial.js
+++ Scalar-Vector Multiplication/ScalarVectorTutorial.js
@@ -2,7 +2,7 @@
  * @author Gabe Webb <gwebb@unca.edu>
  * @author Owen Feldmann <ofeldman@unca.edu>
  * Assignment 2: Vector Operations Tutorial Site: Vector Addition
- * Last Modified: 2/11/2022
+ * Last Modified: 2/15/2022
  */
 
 var canvas;
@@ -11,11 +11,10 @@ var canvasWidth;
 var canvasHeight;
 
 /**
- * points[0] is the start point of the first vector.
- * points[1] is the end point of the first vector.
- * points[2] and points[3] coorespond to the second vector.
+ * points[0] is the start point of the vector.
+ * points[1] is the end point of the vector.
  */
-var points = [null, null, null, null];
+var points = [null, null];
 /**
  * points[0][x] gives the x coordinate of the first point
  */
@@ -71,7 +70,7 @@ function clickCanvas(event)
 	}
 	
 	//store coords as next point
-	for(var i = 0; i < points.length-2; i++) // length-2 in order to allow for second vector to be automatically stored
+	for(var i = 0; i < points.length; i++)
 	{
 		if(points[i] != null) continue;//skip to first empty point
 		
@@ -93,8 +92,7 @@ function clickCanvas(event)
 		
 			canvasContext.fillStyle = 'rgba(0,0,255,1.0)';//blue
 			canvasContext.font = '15px Arial';
-			var vectorName = ((i==3) ? "u" : "v");
-			canvasContext.fillText(vectorName, canvasX + pointSize, canvasY + pointSize);
+			canvasContext.fillText("v", canvasX + pointSize, canvasY + pointSize);
 		}
 		
 		canvasContext.fillStyle = 'rgba(0,0,0,1.0)';//black
@@ -102,41 +100,33 @@ function clickCanvas(event)
 		
 		break;
 	}
-	// Generate a random scalar int between 1 and 2
+	
+	//console.log(points);
+	
+	// Generate a random scalar int between -1.5 and 1.5
 	// Could increase but goes off of canvas very easily
-	// scalar = Math.round(Math.random()+1);
-	scalar = Math.random()+1.5;
+	scalar = (Math.random()-0.5) * 3;
 
 	// Draw scaled version of first vector if first vector is complete
-	if(points[1] != null){
-		points[2] = ScalarVectorMultiplication(scalar,points[0]);
-		points[3] = ScalarVectorMultiplication(scalar,points[1]);
+	if(points[points.length-1] != null){
+		var v = PointPointSubtraction(points[0], points[1]);
+		var vK = ScalarVectorMultiplication(scalar, v);
 
 		pointSize = 15;
 		canvasContext.lineWidth = 3;
 		canvasContext.strokeStyle = 'rgb(255,55,0)';//orange
 
 		canvasContext.beginPath();
-		canvasContext.moveTo(points[2][x], 500 - points[2][y]);
-		canvasContext.lineTo(points[3][x], 500 - points[3][y]);
-		canvasContext.stroke();
+			canvasContext.moveTo(canvasWidth/2, canvasHeight/2);
+			canvasContext.lineTo(canvasWidth/2 + vK[x], canvasHeight/2 - vK[y]);
+			canvasContext.stroke();
 		canvasContext.closePath();
 
 		canvasContext.fillStyle = 'rgb(255,55,0)';//orange
 		canvasContext.font = '15px Arial';
-		canvasContext.fillText("vK", points[3][x] + pointSize, 500 - points[3][y] + pointSize);
+		canvasContext.fillText("vK", canvasWidth/2 + vK[x] + pointSize, canvasHeight/2 - vK[y] + pointSize);
 
-		canvasContext.fillRect(points[3][x] - pointSize/2, 500 - points[3][y] - pointSize/2, pointSize, pointSize);
-	}
-
-
-	//console.log(points);
-	
-	//if all points are placed, draw vector information.
-	if(points[points.length-1] != null)
-	{
-		var v = PointPointSubtraction(points[0],points[1]);
-		var vK = PointPointSubtraction(points[2],points[3]);
+		canvasContext.fillRect(canvasWidth/2 + vK[x] - pointSize/3, canvasHeight/2 - vK[y] - pointSize/3, pointSize, pointSize);
 
 		var text = "v = ["+v[x]+", "+v[y]+", "+v[z]+"], vK = ["+vK[x].toFixed(2)+", "+vK[y].toFixed(2)+", "+vK[z].toFixed(2)+"]" + "    Scalar = "+scalar.toFixed(2);
 		
